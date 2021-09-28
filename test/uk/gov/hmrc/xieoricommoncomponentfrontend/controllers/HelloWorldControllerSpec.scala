1a844e00b7f5d60f52cda27566397f6c5613fe86
NandanaRaghu <60367890+NandanaRaghu@users.noreply.github.com>
2021-09-28 14:39:08 +0100

    ECC-638: Initial Setup (#1)
    * ECC-638: Initial Setup

--- test/uk/gov/hmrc/xieoricommoncomponentfrontend/controllers/HelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/xieoricommoncomponentfrontend/controllers/HelloWorldControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.xieoricommoncomponentfrontend.controllers
 
 import org.scalatest.matchers.should.Matchers
@@ -10,12 +26,10 @@ import play.api.test.Helpers._
 import play.api.inject.guice.GuiceApplicationBuilder
 
 class HelloWorldControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
   override def fakeApplication(): Application =
     new GuiceApplicationBuilder()
-      .configure(
-        "metrics.jvm"     -> false,
-        "metrics.enabled" -> false
-      )
+      .configure("metrics.jvm" -> false, "metrics.enabled" -> false)
       .build()
 
   private val fakeRequest = FakeRequest("GET", "/")
@@ -31,7 +45,7 @@ class HelloWorldControllerSpec extends AnyWordSpec with Matchers with GuiceOneAp
     "return HTML" in {
       val result = controller.helloWorld(fakeRequest)
       contentType(result) shouldBe Some("text/html")
-      charset(result)     shouldBe Some("utf-8")
+      charset(result) shouldBe Some("utf-8")
     }
   }
 }
