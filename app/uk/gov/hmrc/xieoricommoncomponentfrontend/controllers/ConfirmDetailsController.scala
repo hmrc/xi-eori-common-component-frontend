202584e80980e992173dd66975e65c2b455bdfa0
NandanaRaghu <60367890+NandanaRaghu@users.noreply.github.com>
2021-10-19 15:55:41 +0100

    ECC-656: Confirm details page implementation

---
+++ app/uk/gov/hmrc/xieoricommoncomponentfrontend/controllers/ConfirmDetailsController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.xieoricommoncomponentfrontend.controllers
+
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.auth.AuthAction
+import uk.gov.hmrc.xieoricommoncomponentfrontend.domain.LoggedInUserWithEnrolments
+import uk.gov.hmrc.xieoricommoncomponentfrontend.forms.{ConfirmDetailsFormProvider, TradeWithNIFormProvider}
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.TradeWithNI
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.TradeWithNI.{No, Yes}
+import uk.gov.hmrc.xieoricommoncomponentfrontend.views.html.{confirm_details, trade_with_ni}
+
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class ConfirmDetailsController @Inject()(
+  authAction: AuthAction,
+  confirmDetailsView: confirm_details,
+  formProvider: ConfirmDetailsFormProvider,
+  mcc: MessagesControllerComponents
+) extends FrontendController(mcc) with I18nSupport {
+
+  private val form = formProvider()
+
+  // Note: permitted for user with service enrolment
+  def onPageLoad: Action[AnyContent] =
+    authAction.ggAuthorisedUserWithEnrolmentsAction {
+      implicit request => _: LoggedInUserWithEnrolments =>
+        Future.successful(Ok(confirmDetailsView(form)))
+    }
+
+  def submit: Action[AnyContent] = Action { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(formWithErrors => BadRequest(confirmDetailsView(formWithErrors)), value => destinationsByAnswer(value))
+  }
+
+  private def destinationsByAnswer(tradeWithNI: TradeWithNI): Result = tradeWithNI match {
+    case Yes =>
+      Redirect(uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HaveEUEoriController.onPageLoad())
+    case No =>
+      Redirect(uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.XiEoriNotNeededController.eoriNotNeeded())
+  }
+
+}
