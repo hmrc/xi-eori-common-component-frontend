7a0f680dc68628c3f2ad188eab16f7713e0749cf
NandanaRaghu <60367890+NandanaRaghu@users.noreply.github.com>
2021-10-27 15:24:48 +0100

    ECC-656: Changes for trader version of page and fixing tests

---
+++ test/views/ConfirmDetailsViewSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.test.Helpers.contentAsString
+import uk.gov.hmrc.auth.core.AffinityGroup
+import uk.gov.hmrc.xieoricommoncomponentfrontend.forms.{ConfirmDetailsFormProvider, TradeWithNIFormProvider}
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.ConfirmDetails.confirmedDetails
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.{
+  EstablishmentAddress,
+  SubscriptionDisplayResponseDetail,
+  SubscriptionInfoVatId
+}
+import uk.gov.hmrc.xieoricommoncomponentfrontend.viewmodels.ConfirmDetailsViewModel
+import uk.gov.hmrc.xieoricommoncomponentfrontend.views.html.{confirm_details, trade_with_ni}
+import util.ViewSpec
+
+import java.time.LocalDate
+
+class ConfirmDetailsViewSpec extends ViewSpec {
+
+  private implicit val request = withFakeCSRF(fakeRegisterRequest)
+  private val formProvider     = new ConfirmDetailsFormProvider().apply()
+  private def form             = formProvider.bind(Map("value" -> confirmedDetails.toString))
+  private def formWithError    = form.bind(Map("value" -> ""))
+
+  private val response = SubscriptionDisplayResponseDetail(
+    Some("EN123456789012345"),
+    "John Doe",
+    EstablishmentAddress("house no Line 1", "city name", Some("SE28 1AA"), "ZZ"),
+    Some(List(SubscriptionInfoVatId(Some("GB"), Some("999999")), SubscriptionInfoVatId(Some("ES"), Some("888888")))),
+    Some("Doe"),
+    Some(LocalDate.of(1963, 4, 1)),
+    Some("XIE9XSDF10BCKEYAX")
+  )
+
+  private val viewModelOrganisation                     = ConfirmDetailsViewModel(response, AffinityGroup.Organisation)
+  private val confirmDetailsView                        = instanceOf[confirm_details].apply(form, viewModelOrganisation)
+  private val confirmDetailsViewError                   = instanceOf[confirm_details].apply(formWithError, viewModelOrganisation)
+  private lazy val confirmDetailsDoc: Document          = Jsoup.parse(contentAsString(confirmDetailsView))
+  private lazy val confirmDetailsDocWithError: Document = Jsoup.parse(contentAsString(confirmDetailsViewError))
+
+  "Confirm Details page " should {
+
+    "display correct title" in {
+      confirmDetailsDoc.title must startWith("Confirm details")
+    }
+
+    "display correct heading " in {
+      confirmDetailsDoc.body.getElementsByTag("h1").text mustBe "Confirm details"
+    }
+
+    "display errors while empty form is submitted" in {
+      confirmDetailsDocWithError.body.getElementsByClass("govuk-error-summary__list").get(
+        0
+      ).text mustBe "Tell us if these details are correct"
+    }
+
+  }
+
+}
