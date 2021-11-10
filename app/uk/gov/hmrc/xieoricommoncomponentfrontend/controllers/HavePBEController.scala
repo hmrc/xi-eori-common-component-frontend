952f4033b0b6b890e60269c0bca2a471cc3ad6b4
vigneshwaripandi <90615027+vigneshwaripandi@users.noreply.github.com>
2021-11-10 10:04:41 +0000

    ECC-675 Have PBE Address in NI

---
+++ app/uk/gov/hmrc/xieoricommoncomponentfrontend/controllers/HavePBEController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.xieoricommoncomponentfrontend.controllers
+
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.auth.AuthAction
+import uk.gov.hmrc.xieoricommoncomponentfrontend.domain.LoggedInUserWithEnrolments
+import uk.gov.hmrc.xieoricommoncomponentfrontend.forms.HavePBEFormProvider
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.HavePBE
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.HavePBE.{No, Yes}
+import uk.gov.hmrc.xieoricommoncomponentfrontend.views.html.have_pbe
+
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class HavePBEController @Inject() (
+  authAction: AuthAction,
+  havePBEView: have_pbe,
+  formProvider: HavePBEFormProvider,
+  mcc: MessagesControllerComponents
+) extends FrontendController(mcc) with I18nSupport {
+
+  private val form = formProvider()
+
+  // Note: permitted for user with service enrolment
+  def onPageLoad: Action[AnyContent] =
+    authAction.ggAuthorisedUserWithEnrolmentsAction {
+      implicit request => _: LoggedInUserWithEnrolments =>
+        Future.successful(Ok(havePBEView(form)))
+    }
+
+  def submit: Action[AnyContent] = Action { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(formWithErrors => BadRequest(havePBEView(formWithErrors)), value => destinationsByAnswer(value))
+  }
+
+  private def destinationsByAnswer(havePBE: HavePBE): Result = havePBE match {
+    case Yes =>
+      Redirect(uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HaveEUEoriController.onPageLoad())
+    case No =>
+      Redirect(uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.XiEoriNotNeededController.eoriNotNeeded())
+  }
+
+}
---
+++ test/controllers/HavePBEControllerSpec.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import common.pages.RegistrationPage
+import play.api.test.Helpers._
+import uk.gov.hmrc.xieoricommoncomponentfrontend.models.forms.HavePBE
+import util.BaseSpec
+import util.builders.AuthBuilder.withAuthorisedUser
+import util.builders.SessionBuilder
+
+class HavePBEControllerSpec extends BaseSpec {
+
+  "HavePBE controller" should {
+    "return OK and the correct view for a GET" in {
+
+      running(application) {
+        withAuthorisedUser(defaultUserId, mockAuthConnector)
+
+        val request = SessionBuilder.buildRequestWithSessionAndPath(
+          uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HavePBEController.onPageLoad().url,
+          defaultUserId
+        )
+
+        val result = route(application, request).get
+
+        val page = RegistrationPage(contentAsString(result))
+
+        page.title should startWith("Do you have a permanent business establishment in Northern Ireland?")
+      }
+    }
+    "redirect to HaveEuEori Page when Yes is selected" in {
+      running(application) {
+        withAuthorisedUser(defaultUserId, mockAuthConnector)
+
+        val request = SessionBuilder.buildRequestWithSessionAndPathAndFormValues(
+          "POST",
+          uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HavePBEController.submit().url,
+          defaultUserId,
+          Map("value" -> HavePBE.values.head.toString)
+        )
+
+        val result = route(application, request).get
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(
+          result
+        ).get shouldBe uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HaveEUEoriController.onPageLoad().url
+      }
+
+    }
+
+    "redirect to XiEoriNotNeeded Page when No is selected" in {
+      running(application) {
+        withAuthorisedUser(defaultUserId, mockAuthConnector)
+
+        val request = SessionBuilder.buildRequestWithSessionAndPathAndFormValues(
+          "POST",
+          uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HavePBEController.submit().url,
+          defaultUserId,
+          Map("value" -> HavePBE.values.last.toString)
+        )
+
+        val result = route(application, request).get
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(
+          result
+        ).get shouldBe uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.XiEoriNotNeededController.eoriNotNeeded().url
+      }
+
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+
+      running(application) {
+        withAuthorisedUser(defaultUserId, mockAuthConnector)
+
+        val request = SessionBuilder.buildRequestWithSessionAndPathAndFormValues(
+          "POST",
+          uk.gov.hmrc.xieoricommoncomponentfrontend.controllers.routes.HavePBEController.submit().url,
+          defaultUserId,
+          Map("value" -> "")
+        )
+
+        val result = route(application, request).get
+        status(result) shouldBe BAD_REQUEST
+
+        val page = RegistrationPage(contentAsString(result))
+        page.errors should startWith("Select your public business establishment preference")
+      }
+    }
+
+  }
+}
