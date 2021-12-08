471254a303a6c4437259d6fd9b4c88546cddc592
NandanaRaghu <60367890+NandanaRaghu@users.noreply.github.com>
2021-12-08 17:37:40 +0000

    ECC-705: Added tests for address lookup validation

--- test/views/PBEAddressLookupViewSpec.scala
+++ test/views/PBEAddressLookupViewSpec.scala
@@ -28,12 +28,21 @@ class PBEAddressLookupViewSpec extends ViewSpec {
 
   private val view = instanceOf[pbe_address_lookup]
 
-  implicit val request: Request[Any] = withFakeCSRF(fakeRegisterRequest)
-  private val formProvider           = new PBEAddressLookupFormProvider()
-  private def form                   = formProvider.apply()
+  implicit val request: Request[Any]       = withFakeCSRF(fakeRegisterRequest)
+  private val formProvider                 = new PBEAddressLookupFormProvider()
+  private def form                         = formProvider.apply()
+  private def formWithNoPostcodeError      = form.bind(Map("line1" -> "Abc", "postcode" -> ""))
+  private def formWithInvalidPostcodeError = form.bind(Map("line1" -> "Abc", "postcode" -> "PR11UN"))
 
-  private def doc: Document =
-    Jsoup.parse(contentAsString(view(form)))
+  private def formWithline1Error =
+    form.bind(Map("line1" -> "Property name or number cannot exceed 35 characters", "postcode" -> "BT11UN"))
+
+  private def doc: Document = Jsoup.parse(contentAsString(view(form)))
+
+  private def noPostCodeErrorDoc: Document = Jsoup.parse(contentAsString(view(formWithNoPostcodeError)))
+
+  private def invalidPostCodeErrorDoc: Document = Jsoup.parse(contentAsString(view(formWithInvalidPostcodeError)))
+  private def line1LengthErrorDoc: Document     = Jsoup.parse(contentAsString(view(formWithline1Error)))
 
   "Address Lookup Postcode page" should {
 
@@ -60,6 +69,21 @@ class PBEAddressLookupViewSpec extends ViewSpec {
     "display Find Address button" in {
 
       doc.body().getElementsByClass("govuk-button").text() mustBe "Find address"
+    }
+
+    "display errors while empty postcode is submitted" in {
+      noPostCodeErrorDoc.body.getElementsByClass("govuk-list govuk-error-summary__list").get(0)
+        .text mustBe "Enter postal code"
+    }
+
+    "display errors while non BT postcode is submitted" in {
+      invalidPostCodeErrorDoc.body.getElementsByClass("govuk-list govuk-error-summary__list").get(0)
+        .text mustBe "Postcode must start with BT"
+    }
+
+    "display errors while Town Length exceeded" in {
+      line1LengthErrorDoc.body.getElementsByClass("govuk-list govuk-error-summary__list").get(0)
+        .text mustBe "Property name or number cannot have more than 35 characters"
     }
 
   }
